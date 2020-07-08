6b983283033945d3e2088f5f1e5a2023d7bdc9c7
jennifer campero couceiro <jenny270294@gmail.com>
2020-07-08 11:15:33 +0200

    first-commit

---
+++ src/main/java/central/system/api/CentralSystemApiApplication.java
@@ -0,0 +1,13 @@
+package central.system.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentralSystemApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentralSystemApiApplication.class, args);
+	}
+
+}
